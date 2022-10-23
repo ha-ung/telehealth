94a97f71f70cdca1e29800b21541ca303246ab73
RexeonNova <hoclamhuynh@gmail.com>
2022-10-23 14:34:44 +0700

    Migrate to Androidx and Material Design + Update compile SDK to 33

--- app/src/main/java/com/example/mobileproject/SimpleLoginActivity.java
+++ app/src/main/java/com/example/mobileproject/SimpleLoginActivity.java
@@ -1,6 +1,6 @@
 package com.example.mobileproject;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class SimpleLoginActivity extends AppCompatActivity {
