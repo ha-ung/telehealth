af7480113db93de49e72a55616aae63f775642c4
ITITIU19020 <66402103+ITITIU19020@users.noreply.github.com>
2022-11-05 19:04:25 +0700

    Profile fragment layout

--- app/src/main/java/com/example/mobileproject/ProfileFragment.java
+++ app/src/main/java/com/example/mobileproject/ProfileFragment.java
@@ -1,5 +1,7 @@
 package com.example.mobileproject;
 
+import android.annotation.SuppressLint;
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +9,15 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import java.util.Calendar;
+import java.util.Objects;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,6 +25,18 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class ProfileFragment extends Fragment {
+
+    private EditText birthday_input;
+
+    private EditText gender_input;
+
+    private int lastSelectedYear;
+    private int lastSelectedMonth;
+    private int lastSelectedDayOfMonth;
+
+    private RadioGroup radioGroup;
+    private RadioButton radioButton;
+    private Button btnDisplay;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -53,12 +76,70 @@ public class ProfileFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
+        addListenerOnButton();
+
+        this.birthday_input = (EditText) requireView().findViewById(R.id.birthday_input);
+        Button buttonDate = (Button) getView().findViewById(R.id.button_date);
+
+        buttonDate.setOnClickListener(v -> buttonSelectDate());
+
+        // Get Current Date
+        final Calendar c = Calendar.getInstance();
+        this.lastSelectedYear = c.get(Calendar.YEAR);
+        this.lastSelectedMonth = c.get(Calendar.MONTH);
+        this.lastSelectedDayOfMonth = c.get(Calendar.DAY_OF_MONTH);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
+
+    // User click on 'Select Date' button.
+    private void buttonSelectDate() {
+
+        // Date Select Listener.
+        DatePickerDialog.OnDateSetListener dateSetListener = (view, year, monthOfYear, dayOfMonth) -> {
+
+            birthday_input.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
+
+            lastSelectedYear = year;
+            lastSelectedMonth = monthOfYear;
+            lastSelectedDayOfMonth = dayOfMonth;
+        };
+
+        DatePickerDialog datePickerDialog =  new DatePickerDialog(getContext(), android.R.style.Theme_Holo_Light_Dialog_NoActionBar, dateSetListener, lastSelectedYear, lastSelectedMonth, lastSelectedDayOfMonth);
+
+        // Show
+        datePickerDialog.show();
+    }
+
+    public void addListenerOnButton() {
+
+        radioGroup = (RadioGroup) getView().findViewById(R.id.gender_group);
+
+        btnDisplay.setOnClickListener(v -> {
+
+            // get selected radio button from radioGroup
+            int selectedId = radioGroup.getCheckedRadioButtonId();
+
+            // find the radiobutton by returned id
+            radioButton = (RadioButton) getView().findViewById(selectedId);
+
+            gender_input = (EditText) radioButton.getText();
+
+        });
+    }
+
+    public void updateProfile(View view) {
+
+        
+
     }
 }
