615aff69e94d4a66657813e1869ae25989aa776c
ITITIU19020 <66402103+ITITIU19020@users.noreply.github.com>
2022-11-12 06:00:56 +0700

    ChatBox_Testing (2 differents tests)

---
+++ app/src/main/java/com/example/mobileproject/ChannelActivity_Test2.java
@@ -0,0 +1,14 @@
+package com.example.mobileproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChannelActivity_Test2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_channel_test2);
+    }
+}
