50af50195d907d1ed164abdacb1a2eb52d6750cd
ha-ung <ha.ung@katalon.com>
2022-11-24 11:59:09 +0700

    Added chat functionality (real time)

--- app/src/main/java/com/example/mobileproject/PatientMessageFragment.java
+++ app/src/main/java/com/example/mobileproject/PatientMessageFragment.java
@@ -30,8 +30,16 @@ import com.example.mobileproject.db.MonitorDao;
 import com.example.mobileproject.db.TelehealthDatabase;
 import com.example.mobileproject.db.UserDao;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.net.URISyntaxException;
 import java.time.LocalDate;
 import java.util.List;
+
+import io.socket.client.IO;
+import io.socket.client.Socket;
+import io.socket.emitter.Emitter;
 
 public class PatientMessageFragment extends Fragment implements View.OnClickListener {
     public List<Message> messagesList;
@@ -42,6 +50,16 @@ public class PatientMessageFragment extends Fragment implements View.OnClickList
     public Integer userId;
     public String userName;
     public Button sendButton;
+    private io.socket.client.Socket socket;
+    {
+        try {
+            socket = IO.socket("http://10.0.2.2:5000");
+            Log.d("Patient Message Fragment", "Connected to socket successfully.");
+        } catch (URISyntaxException e) {
+            Log.d("Patient Message Fragment", "Error connecting to socket.");
+        }
+    }
+
 
     public PatientMessageFragment() {
 
@@ -62,6 +80,13 @@ public class PatientMessageFragment extends Fragment implements View.OnClickList
     }
 
     @Override
+    public void onDestroy() {
+        super.onDestroy();
+        socket.disconnect();
+
+    }
+
+    @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         TelehealthDatabase appDatabase = TelehealthDatabase.getDbInstance(getActivity());
         MessageDao messageDao = appDatabase.messageDao();
@@ -79,6 +104,69 @@ public class PatientMessageFragment extends Fragment implements View.OnClickList
         recyclerView.scrollToPosition(messagesList.size() - 1);
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        socket.connect();
+        socket.on(Socket.EVENT_CONNECT, onConnect);
+        socket.on("update_chat", onUpdate);
+    }
+
+    private Emitter.Listener onConnect = new Emitter.Listener() {
+        @Override
+        public void call(Object... args) {
+            JSONObject data = new JSONObject();
+            try {
+                data.put("userName", userName);
+                data.put("caseId", caseId);
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+            }
+            socket.emit("join", data);
+        }
+    };
+
+    private Emitter.Listener onUpdate = new Emitter.Listener() {
+        @Override
+        public void call(Object... args) {
+            JSONObject data = (JSONObject) args[0];
+            String text;
+            Integer caseId;
+            String userName2 = "";
+            Integer userId;
+            Message message = new Message();
+            try {
+                userId = data.getInt("userId");
+                text = data.getString("text");
+                caseId = data.getInt("caseId");
+                userName2 = data.getString("userName");
+
+                message.caseId = caseId;
+                message.userName = userName2;
+                message.userId = userId;
+                message.text = text;
+                message.date = String.valueOf(LocalDate.now());
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            Boolean a = userName2.equals(userName);
+            Log.d("userName2", userName2);
+            Log.d("userName2 equals userName", a.toString());
+
+            if (!userName2.equals(userName)) {
+                addItem(message);
+            }
+        }
+    };
+
+    private void addItem(Message message) {
+        getActivity().runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                messagesList.add(message);
+                adapter.setListContent(messagesList);
+                //recyclerView.scrollToPosition(messagesList.size() - 1);
+            }
+        });
     }
 
     @Override
@@ -102,6 +190,18 @@ public class PatientMessageFragment extends Fragment implements View.OnClickList
         adapter.setListContent(messagesList);
         recyclerView.scrollToPosition(messagesList.size() - 1);
         messageInput.setText("");
+
+        JSONObject data = new JSONObject();
+        try {
+            data.put("userId", userId);
+            data.put("userName", userName);
+            data.put("text", text);
+            data.put("caseId", caseId);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        socket.emit("message", data);
 
         InputMethodManager inputManager = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
         inputManager.hideSoftInputFromWindow(getActivity().getCurrentFocus().getWindowToken(),InputMethodManager.HIDE_NOT_ALWAYS);
